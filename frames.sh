7606bd1012bfda8bc3c7bd3df3a8e011d1f650a0
josephcappadona <josephcappadona27@gmail.com>
2018-09-23 06:50:47 -0400

    Added labels directory

--- frames.sh
+++ frames.sh
@@ -3,8 +3,10 @@ FILE_PATH=$1
 ARGS="${@:2}"
 FILE_NAME=$(basename -- "$FILE_PATH")
 VIDEO_NAME="${FILE_NAME%%.*}"
-DIR="images/$VIDEO_NAME"
-mkdir -p $DIR
-rm -f $DIR/*
-ffmpeg -i $FILE_PATH $ARGS $DIR/image-%06d.png -hide_banner
+FRAMES_DIR="images/$VIDEO_NAME"
+LABELS_DIR="labels/$VIDEO_NAME"
+mkdir -p $FRAMES_DIR
+mkdir -p $LABELS_DIR
+rm -f $FRAMES_DIR/*
+ffmpeg -i $FILE_PATH $ARGS $FRAMES_DIR/image-%06d.png -hide_banner
 
