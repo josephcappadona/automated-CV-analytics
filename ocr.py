2dd680a4c7d3b585f46f22ee955260512a5757c0
josephcappadona <josephcappadona27@gmail.com>
2018-09-23 10:12:51 -0400

    Added trimming to OCR preprocessing

--- ocr.py
+++ ocr.py
@@ -1,18 +1,27 @@
 import pytesseract
-from PIL import Image, ImageEnhance, ImageOps, ImageFilter
+from PIL import Image, ImageEnhance, ImageOps, ImageFilter, ImageChops
 
 def image_to_text(im):
 
-    new_size = tuple(3*x for x in im.size)
+    new_size = tuple(4*x for x in im.size)
     im = im.resize(new_size, Image.ANTIALIAS)
 
+    im = im.filter(ImageFilter.MedianFilter())
     enhancer = ImageEnhance.Contrast(im)
     im = enhancer.enhance(2)
     im = im.convert('L')
     im = ImageOps.invert(im)
     im = im.point(lambda x: 0 if x < 100 else 255)
     im = im.filter(ImageFilter.SMOOTH_MORE)
+    im = trim(im, 255)
 
-    return pytesseract.image_to_string(im, config='-psm 6'), im
+    return pytesseract.image_to_string(im, lang='eng', config='-psm 6'), im
 
 
+# taken from https://gist.github.com/mattjmorrison/932345
+def trim(im, border):
+    bg = Image.new(im.mode, im.size, border)
+    diff = ImageChops.difference(im, bg)
+    bbox = diff.getbbox()
+    if bbox:
+        return im.crop(bbox)
