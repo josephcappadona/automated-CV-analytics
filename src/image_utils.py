4d6c10a3525aaa118b3ff1fa21c765a82b8dd399
josephcappadona <josephcappadona27@gmail.com>
2018-09-26 16:23:56 -0400

    Fine tuned image_preprocess for ACR text

--- src/image_utils.py
+++ src/image_utils.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageEnhance, ImageOps, ImageFilter, ImageChops
 
 
+# crop image to remove empty space around text
 # taken from https://gist.github.com/mattjmorrison/932345
 def trim(im, border_color):
     bg = Image.new(im.mode, im.size, border_color)
@@ -10,17 +11,20 @@ def trim(im, border_color):
         return im.crop(bbox)
 
 
+# currently configured specifically for ACR text
 def preprocess_image(im):
+    
     new_size = tuple(4*x for x in im.size)
-    im = im.resize(new_size, Image.ANTIALIAS)
+    im = im.resize(new_size, Image.BICUBIC)
 
-    enhancer = ImageEnhance.Contrast(im)
-    im = enhancer.enhance(2)
-    im = im.convert('L')
-    im = ImageOps.invert(im)
-    im = im.point(lambda x: 0 if x < 75 else 255)
-    im = im.filter(ImageFilter.SMOOTH_MORE)
-    im = trim(im, 255)
+    im = im.convert('L') # make monochrome
+    im = ImageOps.invert(im) # make text background white and text black
+    im = im.point(lambda x: 0 if x < 100 else 255) # binarize
 
+    # invert image if background is black
+    if im.getpixel((0,0)) < 80:
+        im = ImageOps.invert(im)
+    im = trim(im, im.getpixel((0,0)))
+ 
     return im
 
