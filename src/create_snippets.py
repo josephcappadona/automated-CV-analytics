eb664a82aa0196831fee8b627b1d14c2f0f474c7
Joseph Cappadona <josephcappadona27@gmail.com>
2019-02-11 03:24:51 -0500

    Altered negative snippet creation algorithm

--- src/create_snippets.py
+++ src/create_snippets.py
@@ -4,6 +4,7 @@ import glob2
 import xml.etree.ElementTree as ET
 from PIL import Image
 from snippets import create_snippet, create_negative_snippets
+from collections import defaultdict
 
 
 def get_label_fps(dir_):
@@ -33,7 +34,7 @@ def get_boxes(label_fp):
     tree = ET.parse(label_fp)
     xml_root = tree.getroot()
 
-    boxes = {}
+    boxes = defaultdict(list)
     for child in xml_root:
         if child.tag == 'object':
             for attrib in child:
@@ -41,7 +42,7 @@ def get_boxes(label_fp):
                     name = attrib.text
                 elif attrib.tag == 'bndbox':
                     xmin, ymin, xmax, ymax = [int(coord.text) for coord in attrib]
-            boxes[name] = (xmin-1, ymin-1, xmax-1, ymax-1) # subtract 1 b/c LabelImg does not 0-index
+            boxes[name].append((xmin-1, ymin-1, xmax-1, ymax-1)) # subtract 1 b/c LabelImg does not 0-index
     return boxes
 
 
@@ -49,26 +50,35 @@ if __name__ == '__main__':
 
     args = sys.argv
 
-    if len(args) != 3:
-        print('USAGE:  python create_text_snippets.py FRAMES_AND_LABELS_DIR\n\nExample:  python create_text_snippets.py output/my_video/frames')
+    if len(args) < 3 or len(args) > 4:
+        print('USAGE:  python create_text_snippets.py FRAMES_AND_LABELS_DIR VIDEO_NAME [NEG_TO_POS_RATIO]\n\nIf arg is not specified:\nNEG_TO_POS_RATIO=1\n\nExample:  python create_text_snippets.py output/my_video/frames my_video')
         exit(1)
     data_dir = args[1]
-    video_name = data_dir.split('/')[-2]
+    video_name = args[2]
+    neg_to_pos_ratio = int(args[3]) if len(args) == 4 else 1
 
     label_img_fp_pairs = get_label_img_fp_pairs(data_dir)
     print('%d labeled images found.' % len(label_img_fp_pairs))
 
     print('Creating snippets...')
-    count = 0
+    pos_count = 0
+    neg_count = 0
     for label_fp, img_fp in label_img_fp_pairs:
         print(label_fp, img_fp)
 
         boxes = get_boxes(label_fp)
         im = Image.open(img_fp)
+        print(boxes)
         
-        for box_name, box_coords in boxes.items():
-            if create_snippet(video_name, im, box_name, box_coords):
-                count += 1
-        n_neg = create_negative_snippets(video_name, im, img_fp, boxes)
-        count += n_neg
-    print('%d snippets saved to \'output/%s/snippets\'' % (count, video_name))
+        for box_name, box_coords_list in boxes.items():
+            for box_coords in box_coords_list:
+                if create_snippet(video_name, im, box_name, box_coords):
+                    pos_count += 1
+        n_neg = create_negative_snippets(video_name,
+                                         im,
+                                         img_fp,
+                                         boxes,
+                                         neg_to_pos_ratio=neg_to_pos_ratio)
+        neg_count += n_neg
+    count = pos_count + neg_count
+    print('%d snippets (%d pos, %d neg) saved to \'output/%s/snippets\'' % (count, pos_count, neg_count, video_name))
--- src/snippets.py
+++ src/snippets.py
@@ -15,42 +15,56 @@ def create_snippet(video_name, im, box_name, box_coords, output_dir='output'):
     box_img.save(snippet_fp)
     return True
 
-def create_negative_snippets(video_name, im, im_fp, boxes, output_dir='output', verbose=False):
+def is_blank(im):
+    # TODO: try to simplify using image_utils.trim
+    im = np.array(im)
+    top_left_pixel = im[0, 0]
+    is_not_blank = im[im != top_left_pixel].any()
+    return (not is_not_blank)
 
-    occupied = np.zeros(im.size, dtype=np.uint8)
-    for box_coords in boxes.values():
-        x_min, y_min, x_max, y_max = box_coords
-        occupied[x_min:x_max+1, y_min:y_max+1] = 1 # mark off areas containing label boxes
+def create_negative_snippets(video_name, im, im_fp, boxes, output_dir='output', neg_to_pos_ratio=1, verbose=False):
+
+    # mark off areas containing label boxes
+    occupied = np.zeros(im.size[::-1], dtype=np.uint8)
+    for box_coords_list in boxes.values():
+        for box_coords in box_coords_list:
+            x_min, y_min, x_max, y_max = box_coords
+            occupied[y_min:y_max+1, x_min:x_max+1] = 1
 
     w_im, h_im = im.size
     count = 0
-    for box_name, box_coords in boxes.items():
-        # (try to) make one negative box for each label box, try to make them the same size
-        
-        x_min, y_min, x_max, y_max = box_coords
-        w_box, h_box = (x_max - x_min), (y_max - y_min)
-
-        N = 10
-        for i in range(N):
-            # try at most 10 times to find a viable negative image somewhere random in the image
-            # reduce the negative image box size slightly each iteration to make it more likely to find a negative
-            x_rand = randint(0, w_im - w_box)
-            y_rand = randint(0, h_im - h_box)
-
-            # if new box overlaps with any label box (i.e, there is a 1 present in the array)
-            if occupied[x_rand:x_rand+w_box, y_rand:y_rand+h_box].any():
+    for box_name, box_coords_list in boxes.items():
+        for box_coords in box_coords_list:
+            for k in range(neg_to_pos_ratio):
+                # (try to) make one negative snippet for each positive snippet
                 
-                # reduce box size and try again
-                w_box = int(w_box * 0.9)
-                h_box = int(h_box * 0.9)
-                if i == N-1 and verbose:
-                    print('\tCould not find negative box for label \'%s\' in image \'%s\'.\n' % (box_name, im_fp))
-                continue
+                x_min, y_min, x_max, y_max = box_coords
+                w_box, h_box = (x_max - x_min), (y_max - y_min)
 
-            # found good box!
-            neg_box_coords = (x_rand, y_rand, x_rand + w_box, y_rand + h_box)
-            create_snippet(video_name, im, 'NEGATIVE', neg_box_coords)
-            count += 1
-            break
+                N = 10
+                for i in range(N):
+                    # try at most N times to find a viable negative image somewhere random in the image
+                    x_rand = randint(0, w_im - w_box)
+                    y_rand = randint(0, h_im - h_box)
+                    x_rand_min, x_rand_max = x_rand, x_rand + w_box
+                    y_rand_min, y_rand_max = y_rand, y_rand + h_box
+
+                    # if random bbox overlaps with any label bbox
+                    # or subimage is blank
+                    # then try to find new random bbox
+                    does_overlap_label_bbox = occupied[y_rand_min:y_rand_max,
+                                                       x_rand_min:x_rand_max].any()
+                    subimage = im.crop((x_rand_min, y_rand_min, x_rand_max, y_rand_max))
+                    if does_overlap_label_bbox or is_blank(subimage):
+                        
+                        if i == N-1 and verbose:
+                            print('\tCould not find negative box for label \'%s\' in image \'%s\'.\n' % (box_name, im_fp))
+                        continue
+
+                    # found good box!
+                    neg_box_coords = (x_rand, y_rand, x_rand + w_box, y_rand + h_box)
+                    create_snippet(video_name, im, 'NEGATIVE', neg_box_coords)
+                    count += 1
+                    break
     return count
 
