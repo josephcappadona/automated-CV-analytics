9b226391cee7585bdc9d637b2581a00c03684816
Joseph Cappadona <josephcappadona27@gmail.com>
2019-02-15 06:32:52 -0500

    Cleaned + organized/refactored

--- src/extract_frames.py
+++ src/extract_frames.py
@@ -1,34 +1,42 @@
 import sys
 import os
+import utils
+
+
+usage = \
+'''USAGE:  python extract_frames.py VIDEO_FILEPATH [FFMPEG_ARGS]
+
+Example:  python extract_frames.py my_video.mp4 "-vf fps=5"
+'''
 
 args = sys.argv
-
-usage = "\nUSAGE:  python extract_frames.py VIDEO_FILEPATH [FFMPEG_ARGS]\n\nExample:  python extract_frames.py my_video.mp4 \"-vf fps=5\"\n"
-if len(args) < 2:
+if len(args) < 2 or len(args) > 3:
     print(usage)
     exit()
 
 video_filepath = args[1]
-ffmpeg_args = ' '.join(args[2:])
+ffmpeg_args = args[2]
 
 # parse video filename
-video_filename = video_filepath.split('/')[-1]
-video_name = video_filename[:video_filename.rfind('.')]
+video_filename = utils.get_filename(video_filepath)
+video_name = utils.remove_extension(video_filename)
 
 # make output directory
 output_dir = "output"
-frames_dir = "%s/%s/frames" % (output_dir, video_name)
+frames_dir = os.path.join(output_dir, video_name, 'frames')
 os.makedirs(frames_dir, exist_ok=True)
 
 # build ffmpeg command
-ffmpeg_command = "ffmpeg -i \"{}\" {} \"{}/image-%06d.png\" -hide_banner".format(video_filepath, ffmpeg_args, frames_dir)
+ffmpeg_command = "ffmpeg -i '%s' %s '%s/frame-%%06d.png' -hide_banner" \
+                     % (video_filepath, ffmpeg_args, frames_dir)
 
 # log ffmpeg command
-command_filepath = frames_dir + '/command'
+command_filepath = os.path.join(frames_dir, 'command')
 print('\nLogging ffmpeg command in \'%s\'...' % command_filepath)
 with open(command_filepath, 'w+') as command_file:
     command_file.write(ffmpeg_command)
 
 # run ffmpeg command
-os.system("eval `cat \"%s\"`" % command_filepath)
+os.system(ffmpeg_command)
 print('\n\nFrames written to \'%s\'.\n' % frames_dir)
+
