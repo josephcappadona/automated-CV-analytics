a93b828da9ce90841b45caa9d277c60368b2760d
Joseph Cappadona <josephcappadona27@gmail.com>
2019-03-25 21:30:43 -0400

    Fixed spacing issues, typos, import issues

--- src/localization/localize.py
+++ src/localization/localize.py
@@ -1,16 +1,20 @@
 from ess import ESS
 from f_hat import build_summed_area_table, f_hat
+import sys, os
+import nump as np
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from features import extract_features
+
 
 def localize_w_ESS(model, im, mask=None):
 
-	(bovw_histogram, cluster_matrix), (color_histogram, color_matrix) = \
-		extract_features(im, self.BOVW, self.descriptor_extractor, mask=mask)
+    (bovw_histogram, cluster_matrix), (color_histogram, _) = extract_features(im, model.BOVW, model.descriptor_extractor)
+    histogram = np.vstack([bovw_histogram, color_histogram])
 
-	prediction_text = self.SVM.predict([histogram])[0]
-	prediction_index = self.SVM.label_binarizer_.transform([prediction_text]).indices[0]
+    prediction_text = model.SVM.predict([histogram])[0]
+    prediction_index = model.SVM.label_binarizer_.transform([prediction_text]).indices[0]
 
-	SAT = build_summed_area_table(cluster_matrix, self.SVM.estimators_[prediction_index])
+    SAT = build_summed_area_table(cluster_matrix, model.SVM.estimators_[prediction_index])
 
-	bounding_box = ESS(im, f_hat, SAT)
-	return bounding_box
-
+    bounding_box = ESS(im, f_hat, SAT)
+    return bounding_box
