f44cfebd94620cfdbb08875999e8b14e397fa371
Joseph Cappadona <josephcappadona27@gmail.com>
2019-04-01 04:25:41 -0400

    Completely revamped command line argument parsing with  module

--- src/model/build_model.py
+++ src/model/build_model.py
@@ -1,4 +1,7 @@
+import argparse
 import sys
+import yaml
+import logging
 import utils
 import time
 import glob2
@@ -8,117 +11,59 @@ import warnings; warnings.filterwarnings('ignore') # TODO: filter only warnings 
 from model import Model
 
 
-usage = 'USAGE:  python build_model.py --data_dir DATA_DIR [--config CONFIG.yaml]'
-if len(sys.argv) == 1:
-    print(usage)
-    exit()
+parser = argparse.ArgumentParser(prog=('python %s' % sys.argv[0]), description='Build an image classification model.')
+optional_args = parser._action_groups.pop()
+required_args = parser.add_argument_group('required arguments')
+required_args.add_argument('-d', type=str, help='The location of the training snippets', required=True, metavar='DATA_DIR')
+required_args.add_argument('-c', type=str, help='The location of the config file with the model parameters', required=True, metavar='CONFIG')
+required_args.add_argument('-m', type=str, help='The output location of the created model', required=True, metavar='MODEL_OUTPUT_FP')
+optional_args.add_argument('-v', help='Verbose output (debug)', action='store_true')
+parser._action_groups.append(optional_args)
 
-args = utils.parse_args(sys.argv) # type(args) == defaultdict(str)
+args = parser.parse_args(sys.argv[1:] if len(sys.argv) > 1 else ['-h'])
+data_dir = args.d
+config_fp = args.c
+model_output_fp = args.m
+logging_level = logging.DEBUG if args.v else logging.INFO
+logging.basicConfig(stream=sys.stdout, level=logging_level)
 
-# build local variables from command line arguments
-data_dir = args['data_dir']
-
-# import variables from config
-if 'config' in args:
-    import yaml
-    with open(args['config'], 'rt') as config_file:
-        model_args = yaml.load(config_file)
-        for arg_name, arg in model_args.items():
-            vars()[arg_name] = arg
-
-if 'model_type' not in vars():
-    model_type = 'SVM'
-    model_params = {'kernel': 'linear'}
-if 'model_params' not in vars():
-    model_params = {}
-
-if 'model_output_fp' not in vars():
-    model_output_fp = 'output/model {time}.pkl'
-if '{time}' in model_output_fp:
-    time_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-    model_output_fp = model_output_fp.format(time=time_str)
-        
-
-if 'consider_descriptors' not in vars():
-    consider_descriptors = 1
-
-if 'consider_colors' not in vars():
-    consider_colors = 1
-
-if 'BOVW_clusters' not in vars():
-    BOVW_clusters = [8, 16, 32, 64, 128, 256]
-
-if 'data_transform' not in vars() or data_transform == 'None':
-    data_transform = None
-    data_transform_params = {}
-
-if 'feature_selection' not in vars() or feature_selection == 'None':
-    feature_selection = None
-    feature_selection_params = {}
-
-if 'approximation_kernel' not in vars() or approximation_kernel == 'None':
-    approximation_kernel = None
-    approximation_kernel_params = {}
-
-# if no DATA_DIR specified
-if not data_dir:
-    print('No DATA_DIR specified.\n')
-    print(usage)
-    exit()
+logging.info('Loading config...')
+with open(config_fp, 'rt') as config_file:
+    model_args = yaml.load(config_file)
 
 
-print('\n')
-print('DATA_DIR=%s' % data_dir)
-print('MODEL_OUTPUT_FP=\'%s\'' % model_output_fp)
-print('model_type=%s' % model_type)
-print('model_params=%s' % model_params)
-print('consider_descriptors=%s' % consider_descriptors)
-print('consider_colors=%s' % consider_colors)
-print('BOVW_clusters=%s' % BOVW_clusters)
-print('data_transform=%s' % data_transform)
-print('data_transform_params=%s' % data_transform_params)
-print('feature_selection=%s' % feature_selection)
-print('feature_selection_params=%s' % feature_selection_params)
-print('approximation_kernel_params=%s' % approximation_kernel_params)
-print('\n\n')
+logging.info('Creating new model...')
 
-
-print('Creating new model...\n')
-
-print('Loading data...')
+logging.info('Loading data...')
 im_fps = glob2.glob(os.path.join(data_dir, '**/snippets/**/*.png'))
 if not im_fps:
-    print('ERROR:  DATA_DIR \'%s\' contains no snippets in form \'snippets/*.png\'.\n')
+    logging.error('DATA_DIR \'%s\' contains no snippets in form \'snippets/*.png\'. Exiting...')
     exit() 
 ims = utils.import_images(im_fps)
 im_labels = utils.get_labels_from_fps(im_fps)
 
-print('Building new model...\n')
-model = Model(descriptor_extractors.orb_create) # TODO: allow custom descriptor extractor
+logging.info('Building new model...')
+model = Model()
 
-if consider_descriptors:
-    print('Building BOVW...\n')
-    model.BOVW_create(ims, k=BOVW_clusters, show=False) # TODO: allow custom cluster sizes
+if model_args['consider_descriptors']:
+    logging.info('Building BOVW...')
+    model.BOVW_create(ims,
+                      model_args['descriptor_extractor_type'],
+                      model_args['descriptor_extractor_params'],
+                      model_args['BOVW_size'])
 
-print('Training model...\n')
-model.train(model_type, model_params,
-            ims, im_labels,
-            consider_descriptors, consider_colors,
-            data_transform, data_transform_params,
-            feature_selection, feature_selection_params,
-            approximation_kernel, approximation_kernel_params)
-
-print('Computing validation error...\n')
+logging.info('Training model...')
+model.train(ims, im_labels, **model_args)
+ 
+logging.info('Computing validation error...')
 predictions = model.predict(ims)
 
-num_incorrect = sum(1 if l != p else 0 for (l, p) in zip(im_labels, predictions))
+validation_error  = utils.compute_error(im_labels, predictions)
 num_total = len(im_labels)
-validation_error = num_incorrect / num_total
-print('Validation error: %g (%d/%d)\n\n' % (validation_error, num_incorrect, num_total))
+num_incorrect = int(round(validation_error * num_total))
+logging.info('Validation error: %g (%d/%d)' % (validation_error, num_incorrect, num_total))
 
-print('Saving model...')
-model_output_dir = utils.get_directory(model_output_fp)
-os.makedirs(model_output_dir, exist_ok=True)
-model.save(model_output_fp)
-print('Model saved to \'%s\'.\n' % model_output_fp)
+logging.info('Saving model...')
+utils.save_model(model, model_output_fp)
+logging.info('Model saved to \'%s\'.' % model_output_fp)
 
--- src/model/test_model.py
+++ src/model/test_model.py
@@ -1,58 +1,45 @@
-from sys import argv as args
+import argparse
+import sys
+import logging
 import utils
 import glob2
 import os
 from model import Model
 
 
-usage = \
-'''
-USAGE:  python test_model.py MODEL_INPUT_FP DATA_DIR
-'''
-if len(args) == 1:
-    print(usage)
-    exit()
 
-try:
-    # build local variables from command line arguments
-    model_input_fp = args[1]
-    data_dir = args[2]
+parser = argparse.ArgumentParser(prog=('python %s' % sys.argv[0]), description='Test an image classification model.')
+optional_args = parser._action_groups.pop()
+required_args = parser.add_argument_group('required arguments')
+required_args.add_argument('-d', type=str, help='The location of the testing snippets', required=True, metavar='DATA_DIR')
+required_args.add_argument('-m', type=str, help='The location of the model to test', required=True, metavar='MODEL_FP')
+optional_args.add_argument('-v', help='Verbose output (debug)', action='store_true')
+parser._action_groups.append(optional_args)
 
-except IndexError:
-    # if not MODEL_INPUT_FP or DATA_DIR specified
-    print('\nERROR:  Command line arguments incomplete.')
-    print(usage)
-    exit()
+args = parser.parse_args(sys.argv[1:] if len(sys.argv) > 1 else ['-h'])
+data_dir = args.d
+model_fp = args.m
+logging_level = logging.DEBUG if args.v else logging.INFO
+logging.basicConfig(stream=sys.stdout, level=logging_level)
 
+logging.info('Loading model from file...')
+model = Model.load(model_fp)
 
-print('\n')
-print('DATA_DIR=%s' % data_dir)
-print('MODEL_INPUT_FP=\'%s\'' % model_input_fp)
-print('\n\n')
-
-
-print('Loading model from file...\n')
-model = Model.load(model_input_fp)
-
-print('Loading data...\n')
+logging.info('Loading data...')
 im_fps = glob2.glob(os.path.join(data_dir, '**/snippets/**/*.png'))
 ims = utils.import_images(im_fps)
 im_labels = utils.get_labels_from_fps(im_fps)
     
-print('Testing BOVW+%s model...\n' % model.model_type)
+logging.info('Testing model...')
 predictions = model.predict(ims)
 
-num_correct = sum(1 if l == p else 0 for (l, p) in zip(im_labels, predictions))
+accuracy = 1 - utils.compute_error(im_labels, predictions)
 num_total = len(im_labels)
-accuracy = num_correct / num_total
-
-
-print('Accuracy: %g (%d/%d)\n' % (accuracy, num_correct, num_total))
-print('Incorrectly labeled images:\n')
-for i, (label, prediction) in enumerate(zip(im_labels, predictions)):
-    if label != prediction:
-        print(im_fps[i])
-        print('\tCorrect label: %s' % label)
-        print('\tPredicted label: %s' % prediction)
-        print('')
+num_correct = int(round(accuracy * num_total))
+logging.info('Accuracy: %g (%d/%d)' % (accuracy, num_correct, num_total))
+if accuracy != 1:
+    logging.info('Incorrectly labeled images:')
+    for i, (label, prediction) in enumerate(zip(im_labels, predictions)):
+        if label != prediction:
+            logging.info('img_fp=%s,  label=%s,  prediction=%s' % (im_fps[i], label, prediction))
 
