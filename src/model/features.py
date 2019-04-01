b28aabec16c7a50a8fb6dc3c758e4e29ba001326
Joseph Cappadona <josephcappadona27@gmail.com>
2019-04-01 05:47:33 -0400

    Tentatively added spatial pyramid matching

--- src/model/features.py
+++ src/model/features.py
@@ -1,13 +1,16 @@
 import numpy as np
 import utils
+import cv2
 
 
-def extract_bovw_info(im, cluster_model, keypoints, descriptors, n_bins_per_channel=4, mask=None):
+def extract_bovw_info(im, cluster_model, keypoints, descriptors):
     
     h, w = im.shape[:2]
 
     cluster_matrix = -np.ones((h, w), dtype=np.int16)
     bovw_histogram = np.zeros((cluster_model.n_clusters), dtype=np.uint8)
+    l_1_histograms = np.zeros((2, 2, cluster_model.n_clusters), dtype=np.uint8)
+    l_2_histograms = np.zeros((4, 4, cluster_model.n_clusters), dtype=np.uint8)
     
     clusters = cluster_model.predict(descriptors)
     # iterate over keypoints+descriptors,
@@ -16,7 +19,15 @@ def extract_bovw_info(im, cluster_model, keypoints, descriptors, n_bins_per_chan
         x, y = [int(round(coord)) for coord in kp.pt]
         cluster_matrix[y, x] = c_i
         bovw_histogram[c_i] += 1
-        
+
+        l_1_x, l_1_y = int(x / (w/2)), int(y / (h/2))
+        l_1_histograms[l_1_x, l_1_y, c_i] += 1
+        l_2_x, l_2_y = int(x / (w/4)), int(y / (h/4))
+        l_2_histograms[l_2_x, l_2_y, c_i] += 1
+       
+    l_1_histogram = np.hstack(np.hstack(l_1_histograms))
+    l_2_histogram = np.hstack(np.hstack(l_2_histograms))
+    #bovw_histogram = np.hstack((bovw_histogram, l_1_histogram))
     return bovw_histogram, cluster_matrix
 
 
@@ -38,7 +49,7 @@ def get_bin_for_color(pixel, n_color_channels, n_bins_per_channel):
         # then, bin = 0*(4**0) + 2*(4**1) + 3*(4**2) = 56
     return bin_
 
-def extract_color_info(im, keypoints, n_bins_per_channel=4, mask=None):
+def extract_color_info(im, keypoints, n_bins_per_channel=4):
     
     h, w = im.shape[:2]
     n_color_channels = 1 if len(im.shape) == 2 else im.shape[2]
@@ -57,23 +68,19 @@ def extract_color_info(im, keypoints, n_bins_per_channel=4, mask=None):
         
     return color_histogram, color_matrix
 
-
-def extract_features(im, cluster_model, descriptor_extractor, n_bins_per_channel=4, mask=None, consider_descriptors=True, consider_colors=True):
+def extract_features(im, cluster_model, descriptor_extractor, consider_colors, n_bins_per_channel=4):
 
     # extract keypoints and descriptors
-    keypoints, descriptors = descriptor_extractor.detectAndCompute(im, mask=mask)
+    keypoints, descriptors = descriptor_extractor.detectAndCompute(im, None)
     if not len(keypoints):
         keypoints, descriptors = utils.kp_and_des_for_blank_image(im, descriptor_extractor)
     
     # extract BOVW info
-    if consider_descriptors:
-        bovw_histogram, cluster_matrix = extract_bovw_info(im, cluster_model, keypoints, descriptors, mask=mask)
-    else:
-        bovw_histogram, cluster_matrix = [], []
+    bovw_histogram, cluster_matrix = extract_bovw_info(im, cluster_model, keypoints, descriptors)
 
     # extract color info
     if consider_colors:
-        color_histogram, color_matrix = extract_color_info(im, keypoints, n_bins_per_channel=n_bins_per_channel, mask=mask)
+        color_histogram, color_matrix = extract_color_info(im, keypoints, n_bins_per_channel=n_bins_per_channel)
     else:
         color_histogram, color_matrix = [], []
 
